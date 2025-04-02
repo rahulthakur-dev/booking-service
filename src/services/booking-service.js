fd7232788b0f0efc72dcfe076ec83df498857053
Rahul Thakur <rahulthakur.developer@gmail.com>
2025-04-02 23:29:21 +0530

    add cancelBooking fuction for makePayment function

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -57,9 +57,8 @@ async function makePayment(data) {
         const bookingTime = new Date(bookingDetails.createdAt);
         const currentTime = new Date();
 
-        if (currentTime - bookingTime > 300000) { // 5 minutes in milliseconds
-            await bookingRepository.update(data.bookingId, { status: CANCELLED }, transaction);
-            await transaction.commit(); // Commit the transaction after updating the status
+        if (currentTime - bookingTime > 300000) { 
+            await cancelBooking(data.bookingId); 
             throw new AppError('The booking has expired', StatusCodes.BAD_REQUEST);
         }
 
@@ -81,7 +80,33 @@ async function makePayment(data) {
     }
 }
 
+async function cancelBooking(bookingId) {
+    const transaction = await db.sequelize.transaction();
+    try {
+        const bookingDetails = await bookingRepository.get(bookingId, transaction);
+
+        if (bookingDetails.status === CANCELLED) {
+            await transaction.commit();
+            return true;
+        }
+
+        const url = `${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${bookingDetails.flightId}/seats`;
+        await axios.patch(url, {
+            seats: bookingDetails.noOfSeats,
+            dec: 0,
+        });
+
+        await bookingRepository.update(bookingId, { status: CANCELLED }, transaction);
+        await transaction.commit();
+        return true;
+    } catch (error) {
+        await transaction.rollback();
+        throw error;
+    }
+}
+
 module.exports = {
     createBooking,
     makePayment,
+    cancelBooking,
 };
